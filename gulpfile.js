565a695ae52920c759ade3365a81d10f6f446100
Mihai Daniel Adin <mihaidanieladin@gmail.com>
2020-02-01 14:53:52 +0200

    web-start

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('js', function() {
+  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+    .pipe(gulp.dest("src/js"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('serve', gulp.series('sass', function(){
+  browserSync.init({
+    server: "./src"
+  });
+  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], gulp.series('sass'));
+  gulp.watch("src/*.html").on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.parallel('js','serve'));
